0af701a6cb45a663976ddc52de9d5f4241e05b48
Josif Bakic <josif.bakic@givesense.rs>
2022-08-15 15:33:59 +0200

    Dan 4: Searchbar kompletno optimizovan, uvodjenje ruti

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import React,{useEffect, useState} from 'react';
-import Navbar from './Components/Navbar';
-import './App.css';
-import Table from './Components/Table';
-import { Data } from './api' 
-import Searchbar from './Components/Searchbar';
+import React, { useEffect, useState } from "react";
+import Navbar from "./Components/Navbar";
+import "./App.css";
+import Table from "./Components/Table";
+import { Data } from "./api";
+import Searchbar from "./Components/Searchbar";
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   const [data, setData] = useState<Data[]>([]);
-  const [pretraga, setPretraga] = useState<string>("")
+  const [pretraga, setPretraga] = useState<string>("");
+  const [tip, setTip] = useState<string>("");
 
-  useEffect(()=>{
-    fetch('http://localhost:3001/person')
-    .then(res=>res.json())
-    .then((data: Data[])=>setData(data))
-  },[])
+  useEffect(() => {
+    fetch("http://localhost:3001/person")
+      .then((res) => res.json())
+      .then((data: Data[]) => setData(data));
+  }, []);
 
-  const trazi = () => {
-    let newData;
-    newData = data.filter(x=>x.name.startsWith(pretraga))
-    setData(newData);
-  }
+  const trazi = () => {};
+
+  const prikazTabele = (data: Data[]) => {
+    if (pretraga.length != 0) {
+      let newData;
+      newData = data.filter((x) =>
+        x.name.toLowerCase().startsWith(pretraga.toLowerCase())
+      );
+      if (tip === "") {
+        return newData;
+      } else {
+        return newData.filter((x) => x.userType === tip);
+      }
+    } else {
+      if (tip === "") {
+        return data;
+      } else {
+        return data.filter((x) => x.userType === tip);
+      }
+    }
+  };
 
   return (
     <div className="App">
-      <header className="header">
-        Application
-      </header>
-      <Navbar/>
-      <Searchbar podaci={data} searchCriteria={pretraga} onChangeCriteria={(value)=>setPretraga(value)} search={trazi}/>
-      <Table podaci={data}/>
-      <div>{pretraga}</div>
+      <header className="header">Application</header>
+      <Navbar />
+      <Searchbar
+        podaci={data}
+        searchCriteria={pretraga}
+        changeSearchCriteria={(value) => setPretraga(value)}
+        typeCriteria={tip}
+        changeTypeCriteria={(value) => setTip(value)}
+        search={trazi}
+      />
+      {/* <div>
+      <Routes>
+        <Route path="/" element={<Table podaci={prikazTabele(data)} />}/>
+      </Routes>
+      </div> */}
+      <Table podaci={prikazTabele(data)} />
     </div>
   );
 }
--- src/Components/Searchbar.tsx
+++ src/Components/Searchbar.tsx
@@ -1,37 +1,53 @@
-import { Data } from "../api"
+import { Data } from "../api";
 type Props = {
-    podaci: Data[];
-    searchCriteria: string;
-    onChangeCriteria: (value: string) => void
-    search: () => void;
-}
+  podaci: Data[];
+  searchCriteria: string;
+  changeSearchCriteria: (value: string) => void;
+  typeCriteria: string;
+  changeTypeCriteria: (value: string) => void;
+  search: () => void;
+};
 
-const Searchbar = ({podaci, searchCriteria, onChangeCriteria, search}: Props) => {
-    const tipovi = (podaci: Data[]) => {
-        return podaci.map(x=>x.userType).reduce((acc: string[], curr: string)=>{
-            if(!acc.includes(curr)){
-                acc.push(curr);
-            }
-            return acc;
-        }, [])
-    }
+const Searchbar = ({
+  podaci,
+  searchCriteria,
+  changeSearchCriteria,
+  typeCriteria,
+  changeTypeCriteria,
+  search,
+}: Props) => {
+  const tipovi = (podaci: Data[]) => {
+    return podaci
+      .map((x) => x.userType)
+      .reduce((acc: string[], curr: string) => {
+        if (!acc.includes(curr)) {
+          acc.push(curr);
+        }
+        return acc;
+      }, []);
+  };
 
-    return (
-        <div>
-            <label>Name</label>
-            <input type="text" value={searchCriteria} onChange={(e)=>onChangeCriteria(e.target.value)}>
-
-            </input>
-            <label>User type</label>
-            <select>
-                <option value=" "></option>
-                {tipovi(podaci).map(x=>
-                    (<option value={x}>{x}</option>)
-                )}
-            </select>
-            <button onClick={search}>Search</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <label>Name</label>
+      <input
+        type="text"
+        value={searchCriteria}
+        onChange={(e) => changeSearchCriteria(e.target.value)}
+      ></input>
+      <label>User type</label>
+      <select
+        value={typeCriteria}
+        onChange={(e) => changeTypeCriteria(e.target.value)}
+      >
+        <option value=""></option>
+        {tipovi(podaci).map((x) => (
+          <option value={x}>{x}</option>
+        ))}
+      </select>
+      <button onClick={search}>Search</button>
+    </div>
+  );
+};
 
 export default Searchbar;
